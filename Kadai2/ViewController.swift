3c811483632085a38e48b1314777e1dbc6d4a85f
MasakatsuTagishi <m.t-10.22@docomo.ne.jp>
2022-02-01 13:41:43 +0900

    Initial Commit

---
+++ Kadai2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kadai2
+//
+//  Created by 田岸将勝 on 2022/02/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
