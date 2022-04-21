e26cdfce33c30c7855a567a01bcfc68d89bb6df9
MasakatsuTagishi <m.t-10.22@docomo.ne.jp>
2022-04-21 12:48:27 +0900

    【完成】課題2提出用

--- Kadai2/ViewController.swift
+++ Kadai2/ViewController.swift
@@ -8,10 +8,57 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet private weak var firstTextField: UITextField!
+    @IBOutlet private weak var secondTextField: UITextField!
+    @IBOutlet private weak var segmentedControl: UISegmentedControl!
+    @IBOutlet private weak var label: UILabel!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBAction private func pressButton(_ sender: UIButton) {
+        let calculate = Calculate()
+        let value1 = firstTextField.textToDouble ?? 0
+        let value2 = secondTextField.textToDouble ?? 0
+        let index = segmentedControl.selectedSegmentIndex
+        switch index {
+        case 0:
+            label.text = String(calculate.addition(value1: value1, value2: value2))
+        case 1:
+            label.text = String(calculate.subtract(value1: value1, value2: value2))
+        case 2:
+            label.text = String(calculate.multiply(value1: value1, value2: value2))
+        case 3:
+            if value2 == 0 {
+                label.text = "割る数には0以外を入力してください。"
+            } else {
+                label.text = String(calculate.divide(value1: value1, value2: value2))
+            }
+        default:
+            label.text = "0"
+        }
     }
-    
+}
+
+struct Calculate {
+    func addition(value1: Double, value2: Double) -> Double {
+        value1 + value2
+    }
+
+    func subtract(value1: Double, value2: Double) -> Double {
+        value1 - value2
+    }
+
+    func multiply(value1: Double, value2: Double) -> Double {
+        value1 * value2
+    }
+
+    func divide(value1: Double, value2: Double) -> Double {
+        value1 / value2
+    }
+}
+
+extension UITextField {
+    var textToDouble: Double? {
+        let text = self.text
+        let double = text.flatMap { Double($0) }
+        return double
+    }
 }
